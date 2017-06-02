b75bb3e7d8dfe9e85bf803522dc66593eb479d4e
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-02 05:52:13 -0500

    Prueba Cifrado

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def webhook():
 
     # endpoint for processing incoming messaging events
     flag = False
+    key = ""
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
