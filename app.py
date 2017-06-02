6a4cd979c7f2aaf1dfa85d8e79f9c5add6a51508
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-02 05:30:57 -0500

    Prueba Cifrado

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def webhook():
                         send_menu(sender_id, "What do you want to do next?")
                     elif message_text ==  "Adios":
                         send_message(sender_id, "Di Adios")
-                    elif(flag == True && message_text != ""):
+                    elif(flag == True and message_text != ""):
                         text = message_text
                         send_message(sender_id, EncryptDES)
 
