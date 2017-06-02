e4f378c52fba764c853532faeb31a14e7efa422d
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-02 03:53:41 -0500

    Menu correcion 4

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def webhook():
                     message_text = messaging_event["message"]["text"]  # the message's text
                     if message_text ==  "Hola":
                         send_message(sender_id, "Hola, Soy Cryp2me. Inserta una llave de 8 caracteres")
-                    elif len(message_text) == 8:
+                    elif message_text == "diamante":
                         send_menu(sender_id,"What do you want to do next?")
                     elif message_text ==  "Adios":
                         send_message(sender_id, "Di Adios")
