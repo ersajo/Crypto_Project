831beca06ef2062e6c156e9bf4bd9b39c8ad6d11
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-05 01:58:59 -0500

    Prueba lib bot

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import json
 from flask import Flask, request
 from pymessenger.bot import Bot
 
@@ -7,6 +9,35 @@ app = Flask(__name__)
 ACCESS_TOKEN = os.environ["PAGE_ACCESS_TOKEN"]
 VERIFY_TOKEN = os.environ["VERIFY_TOKEN"]
 bot = Bot(ACCESS_TOKEN)
+
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+@app.route('/', methods=['POST'])
+def webhook():
+    output = request.get_json()
+    log(output)
+    if output["object"] == "page":
+
+        for event in output["entry"]:
+            messaging = events["messaging"]
+            for x in messaging:
+                if x.get("message"):
+                    recipient_id = x['sender']['id']
+                    if x['message'].get('text'):
+                        message = x['message']['text']
+                        bot.send_text_message(recipient_id, message)
+                    else:
+                        pass
+        return "Success"
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -34,6 +65,13 @@ def hello():
                     pass
         return "Success"
 
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
 
-if __name__ == "__main__":
-    app.run(port=5002, debug=True)
+def logs(message):  # simple wrapper for logging to stdout on heroku
+    print message
+    sys.stdout.flush()
+
+if __name__ == '__main__':
+    app.run(debug=True)
