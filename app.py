a541ed69a226f60bc5a71450f5d02262602dec8e
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-06 00:36:09 -0500

    Prueba subir archivo heroku

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ import six
 import requests
 from Crypto.Cipher import DES
 from Crypto import Random
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from pymessenger.bot import Bot
 
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = set(['txt', 'png'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ACCESS_TOKEN = os.environ["PAGE_ACCESS_TOKEN"]
 VERIFY_TOKEN = os.environ["VERIFY_TOKEN"]
@@ -32,16 +36,39 @@ def verify():
 
     return "Hello world", 200
 
-@app.route('/tmp', methods=['GET', 'POST'])
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save('tmp/file.txt')
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 @app.route('/', methods=['POST'])
 def webhook():
     output = request.get_json()
     log(output)
+    upload_file('tmp/file.txt')
     archivo = open('tmp/file.txt', 'r')
     contenido = archivo.read()
     logs("Texto archivo: " + contenido)
