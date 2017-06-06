8908f2211bf9e657f5a456d032d5d4b0b9b1f956
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-06 00:59:38 -0500

    Prueba subir archivo heroku

--- app.py
+++ app.py
@@ -26,9 +26,6 @@ flag_encrypt = False
 flag_decrypt = False
 bot = Bot(ACCESS_TOKEN)
 
-def allowed_file(filename):
-    return '.' in filename and \
-    filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -41,21 +38,28 @@ def verify():
 
     return "Hello world", 200
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/tmp', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+        # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',filename=filename))
+            return redirect(url_for('tmp/file.txt',
+                                    filename=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
