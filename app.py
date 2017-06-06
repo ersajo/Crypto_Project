7b8d0ab88fdc50ee177742a9c663704c53791b4d
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-06 01:41:44 -0500

    Prueba subir archivo bot

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def webhook():
                         if message == 'Hola':
                             bot.send_text_message(recipient_id, "Hi, I'm Crypt2me. Write a 8 characters key...")
                             set_flag(True)
-                        elif message == "prueba":
+                        elif message == "Prueba":
                             EncryptDES('12345678', 'Hola', recipient_id)
                         elif len(key) == 8 and message != 'clear':
                             EncryptDES(key, text, recipient_id)
