af83c9b55bce07363973313b3ed79f95ff25ae50
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-06 20:33:25 -0500

    Cambio chunk por texto

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def webhook():
                         if message == 'Hola':
                             send_text_message(recipient_id, "Hi, I'm Crypt2me. Write a 8 characters key...")
                             set_flag(True)
-                        elif message == "Prueba":
+                        elif message == "proof":
                             EncryptDES('12345678', 'Hola', recipient_id)
                         elif len(key) == 8 and message != 'clear':
                             EncryptDES(key, text, recipient_id)
