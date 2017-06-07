1e5fbed4df48630488b27b3c9550d826cce9977b
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-06 21:13:24 -0500

    Regresando cambios

--- app.py
+++ app.py
@@ -87,8 +87,8 @@ def webhook():
                         if message == 'Hola':
                             send_text_message(recipient_id, "Hi, I'm Crypt2me. Write a 8 characters key...")
                             set_flag(True)
-                        elif message == "prueba":
-                            EncryptDES('12345678', 'DiamanteDiamante', recipient_id)
+                        elif message == "Prueba":
+                            EncryptDES('12345678', 'Hola', recipient_id)
                         elif len(key) == 8 and message != 'clear':
                             EncryptDES(key, text, recipient_id)
                             set_text(message)
@@ -128,7 +128,7 @@ def EncryptDES(key, text, recipient_id):
         while True:
             logs("text: " + text)
             time.sleep(3)
-            if len(text) %16 == 0:
+            if len(text) % 16 == 0:
                 break
             elif len(text) % 16 != 0:
                 text += ' ' * (16 - len(text) % 16)
