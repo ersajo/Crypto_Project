9b4b8f0bc145accd8ca0e14209ba44ac6b276fd1
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-08 14:28:39 -0500

    Prueba log

--- app.py
+++ app.py
@@ -205,11 +205,15 @@ def webhook():
                         logs('Obts: ' + str(obtenido))
                         url = str(obtenido)
                         url = url.split("u'")
+                        j = 0
+                        for i in url:
+                            logs("URL " + str(j) + ": " + str(i))
+                            j += 1
                         url = str(url[5])
                         url = url[:(len(url)-4)]
                         logs("URL: " + url)
-                        #respuesta = DecryptDES('12345678', 4, recipient_id, url)
-                        #send_text_message(recipient_id, respuesta)
+                        respuesta = DecryptDES('12345678', 4, recipient_id, url)
+                        send_text_message(recipient_id, respuesta)
                     else:
                         pass
                 elif x.get("postback"):
