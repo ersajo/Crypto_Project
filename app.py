4ca83c9a32c2b9a550a85c130a4bd533b0e39107
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-08 05:07:23 -0500

    Configurar extract

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def webhook():
                             pass
                     elif x['message'].get('attachments'):
                         URL = x['message']['attachments']
-                        logs('URL: ' + str(URL))
+                        logs('URL: ' + str(URL[1]))
                     else:
                         pass
                 elif x.get("postback"):
