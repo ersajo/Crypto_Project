5695b00d0b88631bc5676b81b142aea5535c9d0c
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-08 04:01:57 -0500

    Prueba DES

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ ALLOWED_EXTENSIONS = set(['txt', 'png'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+key = ''
+text = ''
+status = 'inicio'
+
 @app.route('/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
@@ -100,7 +104,7 @@ def webhook():
                             set_status('key')
                             send_menu(recipient_id, "What do you want to do next?...")
                         else:
-                            pass
+                            logs('wait')
                     else:
                         pass
                 elif x.get("postback"):
