fc044dcdd2b565e5c2b75448c30a69bfb03596d7
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-08 06:46:16 -0500

    Prueba extract

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ def webhook():
                         logs('Obts: ' + str(obtenido))
                         url = str(obtenido)
                         url = url.split("u'")
-                        logs("URL: " + str(url))
+                        logs("URL: " + str(url[4]))
                         #respuesta = DecryptDES('12345678', 4, recipient_id, url)
                         #send_text_message(recipient_id, respuesta)
                     else:
