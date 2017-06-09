d92be1e69928765e82bf9ebc6acc11d417ee54fa
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-09 00:58:30 -0500

    Prueba descrifrado

--- app.py
+++ app.py
@@ -208,7 +208,7 @@ def webhook():
                         j = 0
                         for i in url:
                             logs("URL " + str(j) + ": " + str(i))
-                            logs("cadena: " + url[:5])
+                            logs("cadena: " + str(url[:5]))
                             if(url[:5] == 'https'):
                                 url = url = str(url[j])
                             j += 1
