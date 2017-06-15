f7236d1a9527379c129255e89a59fbe7d4bd3efd
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-15 02:56:24 -0500

    delemitar mensaje

--- app.py
+++ app.py
@@ -254,9 +254,9 @@ def DecryptDES(key, NumBits1, recipient_id, URL):
 
 def EncryptDES(key, text, recipient_id):
     cipher = DES.new(key, DES.MODE_OFB, '12345678')
-    logs("Mensaje: |" + str(text) + "|")
     if len(text) % 8 != 0:
         text += ' ' * (8 - len(text) % 8)
+    logs("Mensaje: |" + str(text) + "|")
     message = cipher.encrypt(text)
     logs("Cifrado: " + str(message))
     logs("Cifrado longitud: " + str(len(message)))
