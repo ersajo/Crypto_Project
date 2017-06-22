fe9bfb15c1e601154d853f878e9b9b968ca17539
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-22 02:45:17 -0500

    Depurar globales

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ perSecBin8 = [20,  1, 10, 47,  7, 29,  4, 16,
                3, 33,  6, 28, 18, 21,  1, 17,
               42, 34, 43, 41, 13, 11, 45, 29,
               36,  3, 32, 27, 21, 40, 12, 31]
-
+global key, BytesUser
 key = ''
 BytesUser = 0
 UPLOAD_FOLDER = 'tmp/'
@@ -164,7 +164,6 @@ def uploaded_file(filename):
 
 @app.route('/', methods=['POST'])
 def webhook():
-    global key, text, BytesUser
     output = request.get_json()
     log(output)
     if output["object"] == "page":
@@ -180,7 +179,6 @@ def webhook():
                             cadena = message[40:]
                             EncryptDES(llave, cadena, recipient_id)
                         elif message[:12] == "Usa la llave":
-                            global BytesUser, key
                             key = message[13:21]
 
                             tmpBytes = ''
@@ -203,7 +201,6 @@ def webhook():
                             send_text_message(recipient_id, 'El formato para descifrar debe ser el siguiente: Usa la llave "Llave de longitud 8" para extraer # bytes. Despues adjunta la imagen en formato.txt')
                             pass
                     elif x['message'].get('attachments') and recipient_id != '430252837348461':
-                        global key, BytesUser
                         obtenido = x['message']['attachments']
                         url = str(obtenido)
                         url = url.split("u'")
